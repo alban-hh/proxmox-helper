50fe394cda7e270301f587662240294bdfa5ec67
Alban Shermadhi <ashermadhi6@gmail.com>
2025-02-02 17:28:41 +0100

    Add color and icon definitions

---
+++ lib/colors.sh
@@ -0,0 +1,16 @@
+YW=$'\033[33m'
+YWB=$'\033[93m'
+BL=$'\033[36m'
+RD=$'\033[01;31m'
+GN=$'\033[1;92m'
+DGN=$'\033[32m'
+BOLD=$'\033[1m'
+CL=$'\033[m'
+
+TAB="  "
+BFR=$'\r\033[K'
+
+CM="${TAB}✔️${TAB}"
+CROSS="${TAB}✖️${TAB}"
+INFO="${TAB}💡${TAB}${CL}"
+HOURGLASS="${TAB}⏳${TAB}"
