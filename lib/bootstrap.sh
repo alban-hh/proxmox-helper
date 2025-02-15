eb95376f5bea2782d7f377e32f6ae66d951f4037
Alban Shermadhi <ashermadhi6@gmail.com>
2025-02-15 13:36:32 +0100

    Add library bootstrap for local and remote execution

---
+++ lib/bootstrap.sh
@@ -0,0 +1,25 @@
+PXH_REPO="${PXH_REPO:-https://raw.githubusercontent.com/alban-hh/proxmox-helper/main}"
+PXH_LIBS=(colors output runtime network packages prompts github docker services backup toolchains addon)
+
+pxh_local_lib_dir() {
+  local src="${BASH_SOURCE[0]:-}"
+  if [[ -n "$src" && -f "$src" && "$src" != /dev/fd/* ]]; then
+    dirname "$src"
+  fi
+}
+
+pxh_load_libs() {
+  local dir lib
+  dir="$(pxh_local_lib_dir)"
+  for lib in "${PXH_LIBS[@]}"; do
+    if [[ -n "$dir" && -f "${dir}/${lib}.sh" ]]; then
+      . "${dir}/${lib}.sh"
+    else
+      . <(curl -fsSL "${PXH_REPO}/lib/${lib}.sh")
+    fi
+  done
+}
+
+pxh_load_libs
+set_std_mode
+enable_error_handling
